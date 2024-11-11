1b87c59fd7d776b4992e83b6c96c653687d46f57
Thitiporn Panthanan <thitiporn.pah@student.mahidol.ac.th>
2024-11-11 12:58:18 +0700

    update server

--- frontend/ClientServer.js
+++ frontend/ClientServer.js
@@ -3,27 +3,14 @@ const express = require('express');
 const path = require('path');
 const dotenv = require('dotenv');
 dotenv.config();
-
+const port =8081
 // Initialize the Express app
 const app = express();
 
-// Connection to MySQL
-const mysql = require('mysql2');
-var db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
-});
-
-// Connect to DB
-db.connect(function(err){
-    if (err) throw err;
-    console.log(`Connected to DB ${process.env.DB_NAME}`);
-});
 
 // Serve static files from the root directory (optional, if you have styles, etc.)
 app.use(express.static(__dirname));
+app.use("/css",express.static(path.join(__dirname,"html/css")));
 
 // Set up routes
 app.get('/', (req, res) => {
@@ -38,15 +25,16 @@ app.get('/login', (req, res) => {
 app.get('/search', (req, res) => {
     res.sendFile(path.join(__dirname, 'html', 'search-result.html'));
 });
-
+app.get('/user-account', (req, res) => {
+    res.sendFile(path.join(__dirname, 'html', 'user_acc_manage.html'));
+});
 // Handle invalid paths
 app.use((req, res, next) => {
     console.log(`Request at ${req.url}`);
     console.log(`404: Invalid accessed`);
     res.sendFile(path.join(__dirname, 'html', 'page not found.html'));
 });
-const PORT = 8000
 // Start the server
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${process.env.PORT}`);
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
